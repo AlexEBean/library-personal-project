10901ab9f73350b57ba310c506fa711f2ae23c03
Alex <alexbean95@gmail.com>
2020-12-10 11:24:04 -0700

    preparing to try hosting

--- server/index.js
+++ server/index.js
@@ -12,10 +12,15 @@ const holdCtrl = require("./controllers/holdController")
 const awsCtrl = require("./controllers/awsController")
 const emailCtrl = require("./controllers/emailController")
 
-const app = express()
-
 const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
 
+const app = express()
+
+const path = require('path')
+
+// const __dirname = path.resolve(path.dirname(''));
+
+app.use(express.static(`${__dirname}/../build`))
 app.use(express.json())
 
 app.use(session({
@@ -64,5 +69,9 @@ app.get("/api/user", checkUser, userCtrl.getUser)
 app.get("/api/users", checkAdmin, userCtrl.getUsers)
 app.put("/api/user/:userId", checkAdmin, userCtrl.updateUser)
 app.delete("/api/user/:userId", checkAdmin, userCtrl.deleteUser)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+  })
 
 app.listen(SERVER_PORT, () => console.log(`Server is listening on port ${SERVER_PORT}`))
