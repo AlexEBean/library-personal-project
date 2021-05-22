6dd6fd0a18e3eae1aa931a086b84ee210e9d04eb
Alex <alexbean95@gmail.com>
2021-05-21 19:19:40 -0600

    This should work definitively now.

--- server/controllers/holdController.js
+++ server/controllers/holdController.js
@@ -22,10 +22,9 @@ module.exports = {
         const db = req.app.get('db')
         const {user_id} = req.session.user
         const {bookId} = req.params
-        const checkHold = (await db.hold.check_hold([+user_id, +bookId]))[0].case
         
         try {
-            if (checkHold === "FALSE"){
+            if ((await db.hold.check_hold([+user_id, +bookId]))[0].case === "FALSE"){
                 await db.hold.add_hold([+user_id, +bookId])
                 res.sendStatus(200)
             } else {
