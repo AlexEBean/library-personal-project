08fdd7b61e307b43c4aa1e57d2bf65e9d6781f4c
Alex <alexbean95@gmail.com>
2021-08-27 16:17:33 -0600

    Testing meta tag for LinkedIn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Nav from "./components/Nav/Nav"
 import routes from './routes.js';
+import libraryHome from "./media"
 
 function App() {
   return (
     <div className = "App">
+      <meta 
+        name = "image" 
+        property = "og:image" 
+        content = {libraryHome}>
+      </meta>
       <Nav/>
       {routes}
     </div>
